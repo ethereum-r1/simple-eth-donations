86c7e79545953003e9d7718f756dcb8567e7c0d9
κασσάνδρα.eth <0xDADA@protonmail.com>
2025-05-02 14:17:46 +0200

    first commit

---
+++ src/EthDonations.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Ownable} from "@solady/contracts/auth/Ownable.sol";
+
+contract EthDonations is Ownable {
+    error DonationsEnded();
+    error DonationsNotEnded();
+    error NoDonation();
+    error TransferFailed();
+    error DonationsGoalReached();
+    error DonationsGoalNotReached();
+    error LengthMismatch();
+    error AmountMismatch();
+
+    event Donation(address indexed donor, uint256 amount);
+
+    uint256 public immutable donationsGoal;
+    uint256 public immutable donationsEndTime;
+
+    mapping(address => uint256) public donations;
+
+    constructor(uint256 _donationsGoal, uint256 _donationsEndTime, address _owner) {
+        donationsGoal = _donationsGoal;
+        donationsEndTime = _donationsEndTime;
+        _initializeOwner(_owner);
+    }
+
+    function donate() external payable {
+        if (block.timestamp > donationsEndTime) revert DonationsEnded();
+        if (msg.value == 0) revert NoDonation();
+        donations[msg.sender] += msg.value;
+
+        emit Donation(msg.sender, msg.value);
+    }
+
+    function returnDonation() external {
+        if (block.timestamp < donationsEndTime) revert DonationsNotEnded();
+        if (address(this).balance >= donationsGoal) revert DonationsGoalReached();
+        uint256 amount = donations[msg.sender];
+        if (amount == 0) revert NoDonation();
+        donations[msg.sender] = 0;
+
+        (bool success,) = msg.sender.call{value: amount}("");
+        if (!success) revert TransferFailed();
+    }
+
+    function claimDonations() external onlyOwner {
+        if (block.timestamp < donationsEndTime) revert DonationsNotEnded();
+        uint256 amount = address(this).balance;
+        if (amount < donationsGoal) revert DonationsGoalNotReached();
+
+        (bool success,) = msg.sender.call{value: amount}("");
+        if (!success) revert TransferFailed();
+    }
+
+    function addToDonations(address[] calldata donors, uint256[] calldata amounts) external payable onlyOwner {
+        if (msg.value == 0) revert NoDonation();
+        if (block.timestamp > donationsEndTime) revert DonationsEnded();
+        uint256 length = donors.length;
+        if (donors.length != amounts.length) revert LengthMismatch();
+
+        uint256 totalAmount = 0;
+        for (uint256 i = 0; i < length; i++) {
+            totalAmount += amounts[i];
+            donations[donors[i]] += amounts[i];
+            emit Donation(donors[i], amounts[i]);
+        }
+
+        if (totalAmount != msg.value) revert AmountMismatch();
+    }
+}
---
+++ test/EthDonations.t.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {EthDonations} from "../src/EthDonations.sol";
+import {Ownable} from "@solady/contracts/auth/Ownable.sol";
+
+contract EthDonationsTest is Test {
+    EthDonations public d;
+
+    function setUp() public {
+        d = new EthDonations(10 ether, block.timestamp + 90 days, msg.sender);
+    }
+
+    function test_Donate() public {
+        d.donate{value: 1 ether}();
+        assertEq(d.donations(address(this)), 1 ether);
+
+        vm.expectRevert(EthDonations.NoDonation.selector);
+        d.donate();
+
+        vm.expectRevert(EthDonations.DonationsEnded.selector);
+        vm.warp(block.timestamp + 90 days + 1);
+        d.donate{value: 1 ether}();
+    }
+
+    function test_ReturnDonation() public {
+        uint256 starting_bal = address(this).balance;
+        d.donate{value: 1 ether}();
+        assertEq(d.donations(address(this)), 1 ether);
+
+        vm.expectRevert(EthDonations.DonationsNotEnded.selector);
+        d.returnDonation();
+
+        vm.warp(block.timestamp + 90 days);
+        d.returnDonation();
+        assertEq(d.donations(address(this)), 0);
+        assertEq(address(this).balance, starting_bal);
+
+        vm.prank(address(1));
+        vm.expectRevert(EthDonations.NoDonation.selector); 
+        d.returnDonation();
+    }
+
+    function test_ClaimDonations() public {
+        d.donate{value: 1 ether}();
+
+        vm.expectRevert(Ownable.Unauthorized.selector);
+        d.claimDonations();
+
+        vm.prank(d.owner());
+        vm.expectRevert(EthDonations.DonationsNotEnded.selector);
+        d.claimDonations();
+
+        vm.warp(block.timestamp + 90 days);
+        vm.prank(d.owner());
+        vm.expectRevert(EthDonations.DonationsGoalNotReached.selector);
+        d.claimDonations();
+
+        d.donate{value: 9 ether}();
+
+        assertEq(address(d).balance, 10 ether);
+
+        vm.expectRevert(EthDonations.DonationsGoalReached.selector);
+        d.returnDonation();
+
+        uint256 bal_before = d.owner().balance;
+
+        vm.prank(d.owner());
+        d.claimDonations();
+
+        assertEq(address(d).balance, 0);
+        
+        uint256 bal_after = d.owner().balance;
+        assertEq(bal_after - bal_before, 10 ether);
+    }
+
+    function test_AddToDonations() public {
+        d.donate{value: 1 ether}();
+        address[] memory donors = new address[](2);
+        donors[0] = address(1);
+        donors[1] = address(this);
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 1 ether;
+        amounts[1] = 1 ether;
+        
+        vm.expectRevert(Ownable.Unauthorized.selector);
+        d.addToDonations(donors, amounts);
+
+        vm.prank(d.owner());
+        vm.expectRevert(EthDonations.NoDonation.selector);
+        d.addToDonations(donors, amounts);
+
+        vm.prank(d.owner());
+        vm.expectRevert(EthDonations.AmountMismatch.selector);
+        d.addToDonations{value: 1 ether}(donors, amounts);
+
+        vm.prank(d.owner());
+        vm.expectRevert(EthDonations.LengthMismatch.selector);
+        address[] memory dummy_donors = new address[](3);
+        d.addToDonations{value: 2 ether}(dummy_donors, amounts);
+        vm.prank(d.owner());
+        d.addToDonations{value: 2 ether}(donors, amounts);
+
+        assertEq(d.donations(address(1)), 1 ether);
+        assertEq(d.donations(address(this)), 2 ether);
+
+        vm.warp(block.timestamp + 90 days + 1);
+
+        vm.prank(address(1));
+        uint256 bal_before = address(1).balance;
+        d.returnDonation();
+        uint256 bal_after = address(1).balance;
+        assertEq(bal_after - bal_before, 1 ether);
+        bal_before = address(this).balance;
+        d.returnDonation();
+        bal_after = address(this).balance;
+        assertEq(bal_after - bal_before, 2 ether);
+
+        vm.prank(d.owner());
+        
+        vm.expectRevert(EthDonations.DonationsEnded.selector);
+        d.addToDonations{value: 2 ether}(donors, amounts);
+    }
+
+    receive() external payable {}
+}
