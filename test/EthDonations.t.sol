315c632a67ec99989f10ea3ff5d4a26a315a30ae
κασσάνδρα.eth <0xDADA@protonmail.com>
2025-05-08 06:52:04 +0200

    fix: rescue ERC20 should be safe transfer

--- test/EthDonations.t.sol
+++ test/EthDonations.t.sol
@@ -229,6 +229,7 @@ contract EthDonationsTest is Test {
         vm.expectRevert(EthDonations.DonationsEnded.selector);
         d.addDonationsFor{value: 2 ether}(donors, amounts);
     }
+    
 
     function test_Fallback() public {
         (bool success,) = address(d).call{value: 1 ether}("");
